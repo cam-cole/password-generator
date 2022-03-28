cd5c674b8bf1a41342b9e6e8a1a91ac9eadce28d
cam-cole <camcole065@gmail.com>
2022-03-27 22:22:27 -0500

    adding criteria validation and finalizing project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,66 @@
-// Set character set and condition prompt variables
-var passwordCharacterSet = [];
+// Set password criteria variables
 var specialCharactersSet = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
 var lowerCaseLettersSet = "abcdefghijklmnopqrstuvwxyz".split("");
 var upperCaseLettersSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var numbersSet = "0123456789".split("");
-
-var specialCharactersPrompt = window.confirm("Would you like to include special characters in your password?");
-var lowerCaseLettersPrompt = window.confirm("Would you like to include lower case letters in your password?");
-var upperCaseLettersPrompt = window.confirm("Would you like to include upper case letters in your password?");
-var numbersPrompt = window.confirm("Would you like to include numbers in your password?");
-var passwordLength = window.prompt("How long would you like your password to be? It must be between 8 and 128 characters.");
 
 // Get Random Number Function
 var getRandomInt = function(max) {
   return Math.floor(Math.random() * max);
 }
 
-// if user confirms a prompt, append the character set to the password character set
-if (specialCharactersPrompt) {
-  passwordCharacterSet.push.apply(passwordCharacterSet,specialCharactersSet);
-}
-
-if (lowerCaseLettersPrompt) {
-  passwordCharacterSet.push.apply(passwordCharacterSet,lowerCaseLettersSet);
-}
-
-if (upperCaseLettersPrompt) {
-  passwordCharacterSet.push.apply(passwordCharacterSet,upperCaseLettersSet);
-}
-
-if (numbersPrompt) {
-  passwordCharacterSet.push.apply(passwordCharacterSet,numbersSet);
-}
-
-console.log(passwordCharacterSet);
-console.log(passwordCharacterSet.length)
-
 // function to generate password
 var generatePassword = function() {
+  // Set empty password set and criteria variables
+  var passwordCharacterSet = [];
+  var specialCharactersPrompt = null;
+  var lowerCaseLettersPrompt = null;
+  var upperCaseLettersPrompt = null;
+  var numbersPrompt = null;
+  var passwordLength = null;
+
+  // password criteria prompts
+  specialCharactersPrompt = window.confirm("Would you like to include special characters in your password?");
+  lowerCaseLettersPrompt = window.confirm("Would you like to include lower case letters in your password?");
+  upperCaseLettersPrompt = window.confirm("Would you like to include upper case letters in your password?");
+  numbersPrompt = window.confirm("Would you like to include numbers in your password?");
+
+  // password criteria validation
+  if (!specialCharactersPrompt && !lowerCaseLettersPrompt && !upperCaseLettersPrompt && ! numbersPrompt) {
+    window.alert("You must select at least one of the password criteria. Please try again.");
+    return generatePassword();
+  }
+  
+  // password length prompt
+  passwordLength = window.prompt("How long would you like your password to be? It must be between 8 and 128 characters.");
+
+  // password length validation
+  if (passwordLength < 8 || passwordLength > 128 || passwordLength === null) {
+    window.alert("Invalid password length input. Please try again.");
+    return generatePassword();
+  }
+
+  // if user confirms a prompt, append the confirmed character set to the password character set
+  if (specialCharactersPrompt) {
+    passwordCharacterSet.push.apply(passwordCharacterSet,specialCharactersSet);
+  }
+
+  if (lowerCaseLettersPrompt) {
+    passwordCharacterSet.push.apply(passwordCharacterSet,lowerCaseLettersSet);
+  }
+
+  if (upperCaseLettersPrompt) {
+    passwordCharacterSet.push.apply(passwordCharacterSet,upperCaseLettersSet);
+  }
+
+  if (numbersPrompt) {
+    passwordCharacterSet.push.apply(passwordCharacterSet,numbersSet);
+  }
+
+  console.log(passwordCharacterSet);
+  console.log(passwordCharacterSet.length)
+  
+  // Create password based on prompts
   password = "";
   for (var i = 0; i < passwordLength; i++) {
     character = passwordCharacterSet[getRandomInt(passwordCharacterSet.length)];
